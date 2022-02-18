5997361b9688ea780dce4dc8d1237c937d19b759
Atamer Atalay <atamer.atalay@signumtte.com>
2022-02-18 10:18:53 +0300

    app module config ayarları (.env)

--- Facility/src/app.module.ts
+++ Facility/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FacilityModule } from './facility/facility.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    FacilityModule,ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://localhost:27017/facility'),
+    FacilityModule, ConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        url: config.get('DATABASE_LINK'),
+      }),
+    }),
   ],
   
 })
