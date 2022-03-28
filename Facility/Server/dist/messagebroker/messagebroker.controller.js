f6f983410c019837fd38c99aceb129e792d9ac7e
sgnm-berkay <ahmet.bozkurt@signumtte.com>
2022-03-28 17:22:00 +0300

    messagebroker updated

--- Facility/Server/dist/messagebroker/messagebroker.controller.js
+++ Facility/Server/dist/messagebroker/messagebroker.controller.js
@@ -20,14 +20,12 @@ const path_enum_1 = require("../common/const/path.enum");
 const kafta_topic_enum_1 = require("../common/const/kafta.topic.enum");
 const classification_history_service_1 = require("../history/classification.history.service");
 const facility_history_service_1 = require("../history/facility.history.service");
-const nestjs_otel_1 = require("nestjs-otel");
 const facilitystructure_history_service_1 = require("../history/facilitystructure.history.service");
 let MessagebrokerController = class MessagebrokerController {
-    constructor(facilityHistoryService, classificationHistoryService, facilityStructureHistoryService, traceService) {
+    constructor(facilityHistoryService, classificationHistoryService, facilityStructureHistoryService) {
         this.facilityHistoryService = facilityHistoryService;
         this.classificationHistoryService = classificationHistoryService;
         this.facilityStructureHistoryService = facilityStructureHistoryService;
-        this.traceService = traceService;
     }
     exceptionListener(message) {
         console.log('this is from message broker exception listener' + message.value);
@@ -71,7 +69,6 @@ __decorate([
     __metadata("design:returntype", Object)
 ], MessagebrokerController.prototype, "loggerListener", null);
 __decorate([
-    (0, nestjs_otel_1.Span)('deneme'),
     (0, microservices_1.EventPattern)(kafta_topic_enum_1.FacilityTopics.FACILITY_OPERATION),
     __param(0, (0, microservices_1.Payload)()),
     __metadata("design:type", Function),
@@ -83,8 +80,7 @@ MessagebrokerController = __decorate([
     (0, nest_keycloak_connect_1.Unprotected)(),
     __metadata("design:paramtypes", [facility_history_service_1.FacilityHistoryService,
         classification_history_service_1.ClassificationHistoryService,
-        facilitystructure_history_service_1.FacilityStructureHistoryService,
-        nestjs_otel_1.TraceService])
+        facilitystructure_history_service_1.FacilityStructureHistoryService])
 ], MessagebrokerController);
 exports.MessagebrokerController = MessagebrokerController;
 //# sourceMappingURL=messagebroker.controller.js.map
--- Facility/Server/src/messagebroker/messagebroker.controller.ts
+++ Facility/Server/src/messagebroker/messagebroker.controller.ts
@@ -6,7 +6,6 @@ import { PathEnums } from 'src/common/const/path.enum';
 import { FacilityTopics } from 'src/common/const/kafta.topic.enum';
 import { ClassificationHistoryService } from 'src/history/classification.history.service';
 import { FacilityHistoryService } from 'src/history/facility.history.service';
-import { Span, TraceService } from 'nestjs-otel';
 import { FacilityStructureHistoryService } from 'src/history/facilitystructure.history.service';
 
 @Controller('messagebroker')
@@ -15,8 +14,7 @@ export class MessagebrokerController {
   constructor(
     private facilityHistoryService: FacilityHistoryService,
     private classificationHistoryService: ClassificationHistoryService,
-    private facilityStructureHistoryService: FacilityStructureHistoryService,
-    private readonly traceService: TraceService,
+    private facilityStructureHistoryService: FacilityStructureHistoryService
   ) {}
 
   @MessagePattern(FacilityTopics.FACILITY_EXCEPTIONS)
@@ -29,7 +27,6 @@ export class MessagebrokerController {
     console.log('this is from message broker logger listener' + message.value);
   }
 
-  @Span('deneme')
   @EventPattern(FacilityTopics.FACILITY_OPERATION)
   async operationListener(@Payload() message): Promise<any> {
     // console.log(message.key);
