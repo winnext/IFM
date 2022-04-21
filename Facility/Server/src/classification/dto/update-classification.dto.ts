1056356699f86a102cb2b27ad579cbd1064da5b2
sgnm-haydar <99257698+sgnm-haydar@users.noreply.github.com>
2022-04-21 16:56:46 +0300

    Delete Facility/Server/src/classification directory
    delete classification to use neo4j

--- Facility/Server/src/classification/dto/update-classification.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateClassificationDto } from './create-classification.dto';
-
-export class UpdateClassificationDto extends PartialType(CreateClassificationDto) {}
