f9e526f27b07813382a7d9940479dd04f14ee625
Atamer Atalay <atamer.atalay@signumtte.com>
2022-05-14 23:46:05 +0300

    Type micro services createType end point

--- winform/Server/src/winform/entities/type.entity.ts
+++ winform/Server/src/winform/entities/type.entity.ts
@@ -1,26 +1,21 @@
-import {SchemaFactory } from '@nestjs/mongoose';
+import { SchemaFactory } from '@nestjs/mongoose';
 import { v4 as uuidv4 } from 'uuid';
-import * as moment from 'moment'
+import * as moment from 'moment';
 export class Type {
-
-  key:string = generateUuid();
-  code:string;
-  name:string;
+  key: string = generateUuid();
+  code: string;
+  name: string;
   tag: string[] = [];
-  label: string;  // not in dtos
+  label: string; // not in dtos
   createdAt: string = moment().format('YYYY-MM-DD HH:mm:ss'); // not in dtos
   updatedAt: string = moment().format('YYYY-MM-DD HH:mm:ss'); // not in dtos
-  isActive: boolean = true;  // not in create dto
+  isActive: boolean = true; // not in create dto
   isDeleted: boolean = false; // not in dtos
 
-
-  hasParent: Boolean = true;  // not in dtos
+  hasParent: Boolean = true; // not in dtos
   labelclass: string;
-  hasType: boolean = false;  // not in dtos
-
-  
-  
+  hasType: boolean = false; // not in dtos
 }
 function generateUuid() {
-  return uuidv4()
+  return uuidv4();
 }
