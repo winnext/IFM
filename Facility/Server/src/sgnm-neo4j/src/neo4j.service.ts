10e45c3ff104e9c40af1006ad3224406b56d5888
Atamer Atalay <atamer.atalay@signumtte.com>
2022-06-04 20:25:28 +0300

    .

--- Facility/Server/src/sgnm-neo4j/src/neo4j.service.ts
+++ Facility/Server/src/sgnm-neo4j/src/neo4j.service.ts
@@ -569,7 +569,7 @@ export class Neo4jService implements OnApplicationShutdown {
         throw new HttpException(has_children_error, 400);
       } else {
         const parent = await this.getParentById(id);
-
+        const deletedNode = await this.updateIsDeletedProp(id, true);
         if (parent) {
           const parent_id = parent["_fields"][0]["properties"].self_id;
           const childrenCount = await this.getChildrenCount(parent_id);
@@ -577,7 +577,7 @@ export class Neo4jService implements OnApplicationShutdown {
             this.updateSelectableProp(parent_id, true);
           }
         }
-        const deletedNode = await this.updateIsDeletedProp(id, true);
+        //////
         return deletedNode;
       }
     } catch (error) {
