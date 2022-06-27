5c4f27d58856bb16e63c11c3fe605d1fc479b673
Atamer Atalay <atamer.atalay@signumtte.com>
2022-06-27 15:00:01 +0300

    facility structure refactory

--- Facility/Server/src/facility-structures/entities/facility-structure.entity.ts
+++ Facility/Server/src/facility-structures/entities/facility-structure.entity.ts
@@ -1,19 +1,4 @@
-import { SchemaFactory } from '@nestjs/mongoose';
-
 import { BaseGraphObject } from 'src/common/baseobject/base.graph.object';
 
 export class FacilityStructure extends BaseGraphObject {
-  type: string;
-  code: string;
-  name: string;
-  description: string;
-  label: string;
-  class_name: string = FacilityStructure.name; // not in dtos
-  labelclass: string;
-  self_id: number; // not in dtos
-  typeId: string;
-  realm: string;
-  optionalLabels: string[]
 }
-
-export const FaciliyStructureSchema = SchemaFactory.createForClass(FacilityStructure);
