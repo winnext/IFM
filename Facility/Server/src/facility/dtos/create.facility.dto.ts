a589adf5b1d5ef2fb4e96ddd4b1f0547a4e34bd4
sgnm-haydar <haydar.urdogan@signumtte.com>
2022-06-27 11:18:53 +0300

    facility updated

--- Facility/Server/src/facility/dtos/create.facility.dto.ts
+++ Facility/Server/src/facility/dtos/create.facility.dto.ts
@@ -1,65 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, ValidateNested } from 'class-validator';
+import { IsNotEmpty, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
-import { Adress } from '../entities/facility.address';
 
-import { ClassificationDetail } from '../entities/classification.detail';
-import { i18nValidationMessage } from 'nestjs-i18n';
-import { CreateFacilityStructureDto } from 'src/facility-structures/dto/create-facility-structure.dto';
-import {
-  LengthWithI18nMessage,
-  i18nValidationMessageEnum,
-  IsNotEmptyWithI18nMessage,
-  IsStringWithI18nMessage,
-} from 'ifmcommon';
+import { TestNode } from '../entities/node.entity';
+
 export class CreateFacilityDto {
   @ApiProperty()
-  @IsNotEmptyWithI18nMessage(i18nValidationMessageEnum.NOT_FOUND)
-  @IsStringWithI18nMessage(i18nValidationMessageEnum.IS_STRING)
-  @LengthWithI18nMessage(i18nValidationMessageEnum.LENGTH, 1, 50)
-  facility_name: string;
+  @ValidateNested({ each: true })
+  @Type(() => TestNode)
+  facilityInfo: TestNode;
 
   @ApiProperty()
-  @IsOptional()
-  @IsStringWithI18nMessage(i18nValidationMessageEnum.IS_STRING)
-  @LengthWithI18nMessage(i18nValidationMessageEnum.LENGTH, 1, 50)
-  locations: string;
+  @ValidateNested({ each: true })
+  @Type(() => TestNode)
+  structureInfo: TestNode;
 
   @ApiProperty()
-  @IsNotEmptyWithI18nMessage(i18nValidationMessageEnum.NOT_FOUND)
-  @IsStringWithI18nMessage(i18nValidationMessageEnum.IS_STRING)
-  @LengthWithI18nMessage(i18nValidationMessageEnum.LENGTH, 1, 50)
-  brand_name: string;
+  @ValidateNested({ each: true })
+  @Type(() => TestNode)
+  assetInfo: TestNode;
 
   @ApiProperty()
-  @IsNotEmptyWithI18nMessage(i18nValidationMessageEnum.NOT_FOUND)
-  @IsStringWithI18nMessage(i18nValidationMessageEnum.IS_STRING)
-  @LengthWithI18nMessage(i18nValidationMessageEnum.LENGTH, 1, 50)
-  type_of_facility: string;
-
-  @ApiProperty()
-  @IsNotEmptyWithI18nMessage(i18nValidationMessageEnum.NOT_FOUND)
-  @IsStringWithI18nMessage(i18nValidationMessageEnum.IS_STRING)
-  @LengthWithI18nMessage(i18nValidationMessageEnum.LENGTH, 1, 50)
+  @IsNotEmpty()
   realm: string;
-
-  @ApiProperty()
-  @IsOptional()
-  @ValidateNested({ each: true })
-  @Type(() => ClassificationDetail)
-  classifications: ClassificationDetail;
-
-  @ApiProperty()
-  @IsNotEmpty({ message: i18nValidationMessage('greet.NOT_EMPTY') })
-  label: string[];
-
-  @ApiProperty()
-  @ValidateNested({ each: true })
-  @Type(() => Adress)
-  address: Adress;
-
-  @ApiProperty()
-  @ValidateNested({ each: true })
-  @Type(() => CreateFacilityStructureDto)
-  structure: CreateFacilityStructureDto;
 }
--- Facility/Server/src/facility/repositories/facility.repository.ts
+++ Facility/Server/src/facility/repositories/facility.repository.ts
@@ -9,54 +9,55 @@ import { Facility } from '../entities/facility.entity';
 import { FacilityStructuresService } from 'src/facility-structures/facility-structures.service';
 import { CreateFacilityStructureDto } from 'src/facility-structures/dto/create-facility-structure.dto';
 import { BaseInterfaceRepository } from 'src/common/interface/base.facility.interface';
+import { Neo4jService } from 'src/sgnm-neo4j/src';
+import { assignDtoPropToEntity } from 'sgnm-neo4j/dist';
+import { createDynamicCyperCreateQuery } from 'src/common/func/neo4js.func';
 
 @Injectable()
 export class FacilityRepository implements BaseInterfaceRepository<Facility> {
   constructor(
-    @InjectModel(Facility.name) private readonly facilityModel: Model<Facility>,
     private facilityStructureService: FacilityStructuresService,
+    private readonly neo4jService: Neo4jService,
   ) {}
 
   async findOneByRealm(realm: string): Promise<Facility> {
-    const facility = await this.facilityModel.findOne({ realm }).exec();
-
-    if (!facility) {
-      throw new FacilityNotFountException(realm);
-    }
-
+    const facility = new Facility();
     return facility;
   }
 
   async create(createFacilityDto: CreateFacilityDto) {
-    const structure = createFacilityDto.structure as CreateFacilityStructureDto;
+    const facility = new Facility();
+    const structure = new Facility();
+    const asset = new Facility();
+    const { structureInfo, facilityInfo, assetInfo, realm } = createFacilityDto;
 
-    const facility = new this.facilityModel(createFacilityDto);
-    await facility.save();
-    await this.facilityStructureService.create(structure);
-    delete createFacilityDto['structure'];
+    const finalFacilityObject = assignDtoPropToEntity(facility, facilityInfo);
+    const finalStructureObject = assignDtoPropToEntity(structure, structureInfo);
+    const finalAssetObject = assignDtoPropToEntity(asset, assetInfo);
+    const facilityQuery = createDynamicCyperCreateQuery(finalFacilityObject, [realm, 'Root']);
+    const structureQuery = createDynamicCyperCreateQuery(finalStructureObject, [realm, 'Structure']);
+    const assetQuery = createDynamicCyperCreateQuery(finalAssetObject, [realm, 'Asset']);
 
-    return facility;
+    //create  node with multi or single label cyper query
+    const facilityNode = await this.neo4jService.write(facilityQuery, finalFacilityObject);
+    const structureNode = await this.neo4jService.write(structureQuery, finalStructureObject);
+    const assetNode = await this.neo4jService.write(assetQuery, finalAssetObject);
+    console.log(structureNode['records'][0]['_fields'][0].identity.low);
+    await this.neo4jService.addRelations(
+      structureNode['records'][0]['_fields'][0].identity.low,
+      facilityNode['records'][0]['_fields'][0].identity.low,
+    );
+    await this.neo4jService.addRelations(
+      assetNode['records'][0]['_fields'][0].identity.low,
+      facilityNode['records'][0]['_fields'][0].identity.low,
+    );
+    return facilityNode['records'][0]['_fields'][0];
   }
   async update(_id: string, updateFacilityDto: UpdateFacilityDto) {
-    await this.findOneById(_id);
-    const updatedFacility = await this.facilityModel
-      .findOneAndUpdate({ _id }, { $set: updateFacilityDto }, { new: true })
-      .exec();
-
-    if (!updatedFacility) {
-      throw new FacilityNotFountException(_id);
-    }
-
-    return updatedFacility;
+    return 'updatedFacility';
   }
 
-  async findOneById(id: string): Promise<Facility> {
-    const facility = await this.facilityModel.findById({ _id: id }).exec();
-
-    if (!facility) {
-      throw new FacilityNotFountException(id);
-    }
-
-    return facility;
+  async findOneById(id: string): Promise<any> {
+    return 'facility';
   }
 }
