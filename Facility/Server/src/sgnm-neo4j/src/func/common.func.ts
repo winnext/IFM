5f3e4d13545841097cbe719a46ac386f7924f90c
Atamer Atalay <atamer.atalay@signumtte.com>
2022-07-04 11:32:36 +0300

    .

--- Facility/Server/src/sgnm-neo4j/src/func/common.func.ts
+++ Facility/Server/src/sgnm-neo4j/src/func/common.func.ts
@@ -3,9 +3,9 @@ import { startWith } from "rxjs";
 //transfer dto(object come from client) properties to specific node entity object
 export function assignDtoPropToEntity(entity, dto) {
   Object.keys(dto).forEach((element) => {
-
+     
         entity[element] = dto[element];
-
+     
   });
 
   return entity;
