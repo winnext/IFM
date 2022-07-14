68e94bd9307a993f00761f029c6f50d467333212
sgnm-haydar <haydar.urdogan@signumtte.com>
2022-07-14 14:02:50 +0300

    asset update dto

--- Asset/Server/src/asset/dto/update-asset.dto.ts
+++ Asset/Server/src/asset/dto/update-asset.dto.ts
@@ -1,13 +1,19 @@
 import { ApiProperty, OmitType } from '@nestjs/swagger';
-import { IsBoolean, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { CreateAssetDto } from './create-asset.dto';
 import * as moment from 'moment';
 
 export class UpdateAssetDto extends OmitType(CreateAssetDto, ['key']) {
   @ApiProperty()
-  @IsNotEmpty()
+  @IsOptional()
   @IsBoolean()
   isActive: boolean;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  name: string;
+
 
   @IsOptional()
   updatedAt = moment().format('YYYY-MM-DD HH:mm:ss');
