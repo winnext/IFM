04b302ee4ce4b82923e4764e1e46b1dc2b97c426
sgnm-mustafa <mustafa.durmaz@signumtte.com>
2022-07-18 16:43:13 +0300

    Asset client create

--- Asset/Client/src/routes.tsx
+++ Asset/Client/src/routes.tsx
@@ -5,17 +5,9 @@ import AppLayout from "./layouts/App/App";
 
 //pages
 import Dashboard from "./layouts/App/components/Dashboard";
-import Facility from "./pages/Facility";
-import Classifications from "./pages/Classifications/Classifications";
 import { NotFound } from "./layouts/App/pages/NotFound";
-import SetClassification from "./pages/Classifications/SetClassification";
-import FacilityFileImport from "./pages/FacilityFileImport";
-import ClassificationFileImport from "./pages/ClassificationFileImport";
-import FacilityStructure from "./pages/FacilityStructure/FacilityStructure";
-import SetFacilityStructure from "./pages/FacilityStructure/SetFacilityStructure";
+import SetAsset from "./pages/Asset/SetAsset";
 import FormGenerate from "./pages/FormGenerate/FormGenerate";
-import Facility2 from "./pages/Facility2";
-// import Main from './pages/Main';
 
 export default function Router() {
   return useRoutes([
@@ -24,34 +16,9 @@ export default function Router() {
       element: <AppLayout />,
       children: [
         { path: "", element: <Dashboard /> },
-        // { path: "facility", element: <Facility /> },
-        { path: "facility", element: <Facility2 /> },
-        // { path: "classifications", element: <Classifications /> },
-        { path: "classifications", element: <SetClassification /> },
-        // { path: "facilitystructure", element: <FacilityStructure /> },
-        { path: "facilitystructure", element: <SetFacilityStructure /> },
+        { path: "assets", element: <SetAsset /> },
         { path: "formgenerate", element: <FormGenerate />},
       ],
-    },
-    {
-      path: "/classifications",
-      element: <AppLayout />,
-      children: [{ path: ":id", element: <SetClassification /> }],
-    },
-    {
-      path: "/facilitystructure",
-      element: <AppLayout />,
-      children: [{ path: ":id", element: <SetFacilityStructure /> }],
-    },
-    {
-      path: "/facility",
-      element: <AppLayout />,
-      children: [{ path: "fileimport", element: <FacilityFileImport /> }],
-    },
-    {
-      path: "/classifications",
-      element: <AppLayout />,
-      children: [{ path: "fileimport", element: <ClassificationFileImport /> }],
     },
     {
       path: "/formgenerate",
